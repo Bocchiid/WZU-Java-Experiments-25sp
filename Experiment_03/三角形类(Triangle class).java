1d9c8db23111e0a3d9cb72a1f80a9a3a3d5c0a7f
Bocchiid <487883857@qq.com>
2025-06-17 13:34:26 +0800

    Add 三角形类(Triangle class).java

--- Experiment_03/三角形类(Triangle class).java
+++ Experiment_03/三角形类(Triangle class).java
@@ -3,72 +3,91 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         double side1, side2, side3;
-        String color;
-        boolean filled;
+        String color, filled;
 
         side1 = input.nextDouble();
         side2 = input.nextDouble();
         side3 = input.nextDouble();
         color = input.next();
-        filled = input.nextBoolean();
+        filled = input.next();
 
-        Triangle a = new Triangle(side1, side2, side3, color, filled);
-        System.out.println(a);
-        System.out.printf("Area=%.2f\n", a.getArea());
-        System.out.printf("Perimeter=%.2f\n", a.getPerimeter());
+        Triangle triangle = new Triangle(color, filled, side1, side2, side3);
+        System.out.println(triangle);
+        System.out.printf("Area=%.2f\n", triangle.getArea());
+        System.out.printf("Perimeter=%.2f\n", triangle.getPerimeter());
     }
 }
 
-class GeometricObject {
-    String color;
-    boolean filled;
+abstract class GeometricObject {
+    private String color;
+    private boolean filled;
 
-    GeometricObject() {
+    public GeometricObject(String color, String filled) {
+        this.color = color;
 
+        if (filled.equals("true")) {
+            this.filled = true;
+        } else {
+            this.filled = false;
+        }
     }
 
-    GeometricObject(String color, boolean filled) {
-        this.color = color;
-        this.filled = filled;
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isFilled() {
+        return filled;
     }
 }
 
 class Triangle extends GeometricObject {
-    double side1 = 1.0;
-    double side2 = 1.0;
-    double side3 = 1.0;
+    private double side1;
+    private double side2;
+    private double side3;
 
-    Triangle() {
-
+    public Triangle() {
+        super("No Color", "false");
+        side1 = 1.0;
+        side2 = 1.0;
+        side3 = 1.0;
     }
 
-    Triangle(double side1, double side2, double side3) {
-        super();
+    public Triangle(String color, String filled, double side1, double side2, double side3) {
+        super(color, filled);
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
     }
 
-    Triangle(double side1, double side2, double side3, String color, boolean filled) {
-        this(side1, side2, side3);
-        this.color = color;
-        this.filled = filled;
+    public double getSide1() {
+        return side1;
     }
 
-    public double getPerimeter() {
-        return side1 + side2 + side3;
+    public double getSide2() {
+        return side2;
+    }
+
+    public double getSide3() {
+        return side3;
     }
 
     public double getArea() {
-        double p = getPerimeter() / 2.0;
-        double s = Math.sqrt(p * (p - side1) * (p - side2) * (p - side3));
+        double p = (getSide1() + getSide2() + getSide3()) / 2;
+
+        double s = Math.sqrt(p * (p - getSide1()) * (p - getSide2()) * (p - getSide3()));
 
         return s;
     }
 
+    public double getPerimeter() {
+        return getSide1() + getSide2() + getSide3();
+    }
+
+    @Override
     public String toString() {
-        return "Triangle: side1=" + side1 + " side2=" + side2 + " side3=" + side3 +
-                " color=" + color + " filled=" + filled;
+        return String.format("Triangle: side1=%.1f side2=%.1f side3=%.1f color=%s filled=" + isFilled(),getSide1(), getSide2(), getSide3(), getColor());
     }
 }
