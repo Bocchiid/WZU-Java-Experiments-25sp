52e965b8dcbd66564f91dbc80f49d58014744c10
Bocchiid <487883857@qq.com>
2025-06-17 13:40:42 +0800

    Add 简单雇员管理(Employee).java

--- Experiment_03/简单雇员管理(Employee).java
+++ Experiment_03/简单雇员管理(Employee).java
@@ -1,7 +1,8 @@
+import java.util.Iterator;
 import java.util.Vector;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Employee e1 = new Employee("zhangsan", 20, "s101", "d01", 3);
         e1.print();
         System.out.println();
@@ -25,13 +26,17 @@ class Person {
     String name;
     int age;
 
-    Person() {
-
-    }
-
-    Person(String name, int age) {
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
 
@@ -40,53 +45,65 @@ class Employee extends Person {
     String department;
     int level;
 
-    Employee() {
-
-    }
-
-    Employee(String name, int age, String empId, String department, int level) {
+    public Employee(String name, int age, String empId, String department, int level) {
         super(name, age);
         this.empId = empId;
         this.department = department;
         this.level = level;
     }
 
+    public String getEmpId() {
+        return empId;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
     public void print() {
         System.out.println("I am a Employee");
-        System.out.printf("My name is %s,I am %d years old.\n", name, age);
-        System.out.printf("I am a %d level Employee.My employeeNumber is %s.I am working in %s\n",
-                level, empId, department);
+        System.out.printf("My name is %s,I am %d years old.\n", getName(), getAge());
+        System.out.printf("I am a %d level Employee.My employeeNumber is %s.I am working in %s\n", getLevel(), getEmpId(), getDepartment());
     }
 }
 
 class Manager extends Employee {
-    Vector<Employee> subordinate;
+    Vector<Employee> a;
 
-    Manager(String name, int age, String empId, String department, int level) {
+    public Manager(String name, int age, String empId, String department, int level) {
         super(name, age, empId, department, level);
-        subordinate = new Vector<>();
+        a = new Vector<>();
     }
 
     public Vector<Employee> getSubordinates() {
-        return subordinate;
+        return a;
     }
 
+    @Override
     public void print() {
         System.out.println("I am a Manager");
-        System.out.printf("My name is %s,I am %d years old.\n", name, age);
-        System.out.printf("I am a %d level Employee.My employeeNumber is %s.I am working in %s\n",
-                level, empId, department);
-
+        System.out.printf("My name is %s,I am %d years old.\n", getName(), getAge());
+        System.out.printf("I am a %d level Employee.My employeeNumber is %s.I am working in %s\n", getLevel(), getEmpId(), getDepartment());
         System.out.println("My subordinate has:");
 
-        for (int i = 0; i < subordinate.size(); i++) {
-            if (i != 0) {
+        Iterator<Employee> iterator = a.iterator();
+        boolean flag = true;
+
+        while (iterator.hasNext()) {
+            Employee employee = (Employee) iterator.next();
+
+            if (!flag) {
                 System.out.print(",");
             }
 
-            System.out.print(subordinate.get(i).name);
+            System.out.print(employee.getName());
+            flag = false;
         }
 
-        System.out.println(".");
+        System.out.print(".");
     }
 }
