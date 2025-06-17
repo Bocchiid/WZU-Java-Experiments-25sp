e32232a82c8d9c1f33e767dd8c749f2e0b4c2c6a
Bocchiid <487883857@qq.com>
2025-06-17 13:35:29 +0800

    Add 求几何对象的面积之和(Computing the summation of the geometric objects).java

--- Experiment_03/求几何对象的面积之和(Computing the summation of the geometric objects).java
+++ Experiment_03/求几何对象的面积之和(Computing the summation of the geometric objects).java
@@ -1,75 +1,100 @@
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
-    public static double sumArea(GeometricObject a[]) {
-        double sum = 0;
-
-        for (int i = 0; i < a.length; i++) {
-            sum += a[i].computeArea();
-        }
-
-        return sum;
-    }
+    /** 自定义函数 */
+//    /** static 方法是静态的, 方法是属于类的 */
+//    public static double sumArea(GeometricObject[] a) {
+//        double sumArea = 0;
+//
+//        for (GeometricObject it : a) {
+//            sumArea += it.calArea();
+//        }
+//
+//        return sumArea;
+//    }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        GeometricObject a[] = new GeometricObject[4];
-        double length, width;
-        double radius;
+
+        GeometricObject[] a = new GeometricObject[4];
 
         for (int i = 0; i < 2; i++) {
+            double length, width;
+
             length = input.nextDouble();
             width = input.nextDouble();
+
             a[i] = new Rectangle(length, width);
         }
 
         for (int i = 2; i < 4; i++) {
+            double radius;
+
             radius = input.nextDouble();
+
             a[i] = new Circle(radius);
         }
 
-        System.out.printf("%.2f\n", sumArea(a));
+        System.out.printf("%.2f\n", GeometricObject.sumArea(a));
     }
 }
 
 abstract class GeometricObject {
-    GeometricObject() {
+    public GeometricObject() {
 
     }
 
-    public abstract double computeArea();
+    public abstract double calArea();
+    /** static 方法是静态的, 方法是属于类的 */
+    public static double sumArea(GeometricObject[] a) {
+        double sumArea = 0;
+
+        for (GeometricObject it : a) {
+            sumArea += it.calArea();
+        }
+
+        return sumArea;
+    }
 }
 
 class Rectangle extends GeometricObject {
-    double length;
-    double width;
+    private double length;
+    private double width;
 
-    Rectangle() {
-
-    }
-
-    Rectangle(double length, double width) {
+    public Rectangle(double length, double width) {
+        super();
         this.length = length;
         this.width = width;
     }
 
-    public double computeArea() {
-        return length * width;
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double calArea() {
+        return getLength() * getWidth();
     }
 }
 
 class Circle extends GeometricObject {
-    double radius;
+    private double radius;
 
-    Circle() {
-
-    }
-
-    Circle(double radius) {
+    public Circle(double radius) {
+        super();
         this.radius = radius;
     }
 
-    public double computeArea() {
-        return Math.PI * radius * radius;
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double calArea() {
+        return Math.PI * getRadius() * getRadius();
     }
 }
