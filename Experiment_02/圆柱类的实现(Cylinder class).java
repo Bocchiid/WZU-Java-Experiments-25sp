5052c2270eb95dcd0324e73bab748ded3856da92
Bocchiid <487883857@qq.com>
2025-06-17 13:23:30 +0800

    Add 圆柱类的实现(Cylinder class).java

--- Experiment_02/圆柱类的实现(Cylinder class).java
+++ Experiment_02/圆柱类的实现(Cylinder class).java
@@ -3,25 +3,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int h, r;
 
-        r = in.nextInt();
-        h = in.nextInt();
-        Cylinder x = new Cylinder(r, h);
-        System.out.printf("%.2f\n",  x.getVolume());
+        int radius, height;
+        radius = in.nextInt();
+        height = in.nextInt();
+
+        Cylinder cylinder = new Cylinder(radius, height);
+        System.out.printf("%.2f\n", cylinder.getVolume());
     }
 }
 
 class Cylinder {
-    int radius;
-    int height;
+    private int radius;
+    private int height;
 
-    Cylinder(int r, int h) {
-        radius = r;
-        height = h;
+    public Cylinder(int radius, int height) {
+        this.radius = radius;
+        this.height = height;
     }
 
-    double getVolume() {
-        return 3.14 * radius * radius * height;
+    public int getRadius() {
+        return radius;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        double PI = 3.14;
+
+        double v = PI * getRadius() * getRadius() * getHeight();
+
+        return v;
     }
 }
