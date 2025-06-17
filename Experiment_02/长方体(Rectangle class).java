9d622674cd5b6c0d066c74eeb44ee86569905b49
Bocchiid <487883857@qq.com>
2025-06-17 13:22:18 +0800

    Add 长方体(Rectangle class).java

--- Experiment_02/长方体(Rectangle class).java
+++ Experiment_02/长方体(Rectangle class).java
@@ -3,33 +3,56 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
         int a, b, c;
 
         a = in.nextInt();
         b = in.nextInt();
         c = in.nextInt();
-        Cuboid x = new Cuboid(a, b, c);
-        x.getBottomArea();
-        x.getVolume();
+
+        Cuboid cuboid = new Cuboid(a, b, c);
+        System.out.println(cuboid.getBottomArea());
+        System.out.println(cuboid.getVolume());
     }
 }
 
 class Cuboid {
-    int length;
-    int width;
-    int height;
+    private int length;
+    private int width;
+    private int height;
+    /** Constructor */
+    public Cuboid() {
 
-    Cuboid(int length, int width, int height) {
+    }
+    /** Overload */
+    public Cuboid(int length, int width, int height) {
         this.length = length;
         this.width = width;
         this.height = height;
     }
-
-    void getBottomArea() {
-        System.out.println(length * width);
+    /** Member Function */
+    /** Setter */
+    public void setLength(int length) {
+        this.length = length;
     }
 
-    void getVolume() {
-        System.out.println(length * width * height);
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getBottomArea() {
+        int s = length * width;
+
+        return s;
+    }
+
+    public int getVolume() {
+        int v = length * width * height;
+
+        return v;
     }
 }
