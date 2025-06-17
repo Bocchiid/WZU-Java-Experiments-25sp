42446ac0eeb4eb153496b2cb3b36509a55eb0a35
Bocchiid <487883857@qq.com>
2025-06-17 13:42:51 +0800

    Add 接口火车(interface train).java

--- Experiment_03/接口火车(interface train).java
+++ Experiment_03/接口火车(interface train).java
@@ -1,44 +1,51 @@
 class Car implements Auto {
-    Car() {
+    public Car() {
 
     }
 
+    @Override
     public void move() {
         System.out.println("I am running very fast.");
     }
 
+    @Override
     public void stop() {
         System.out.println("I am stopping.");
     }
 
+    @Override
     public void dudu() {
         System.out.println("I am a car!Get out Every one!");
     }
 }
 
 class Truck implements Auto {
-    Truck() {
+    public Truck() {
 
     }
 
+    @Override
     public void move() {
         System.out.println("I am running very smoothly.");
     }
 
+    @Override
     public void stop() {
         System.out.println("I am slowly stopping.");
     }
 
+    @Override
     public void dudu() {
         System.out.println("I am a big Auto!Get out Every one and small car!");
     }
 }
 
 class FireTruck extends Truck implements Sprinkler {
-    FireTruck() {
+    public FireTruck() {
 
     }
 
+    @Override
     public void watering() {
         System.out.println("I am watering,No fire!");
     }
