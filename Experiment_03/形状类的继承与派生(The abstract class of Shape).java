35086115d2ae1378474c5242ec277c65b1c6a031
Bocchiid <487883857@qq.com>
2025-06-17 13:39:02 +0800

    Add 形状类的继承与派生(The abstract class of Shape).java

--- Experiment_03/形状类的继承与派生(The abstract class of Shape).java
+++ Experiment_03/形状类的继承与派生(The abstract class of Shape).java
@@ -1,33 +1,43 @@
 class Rectangle extends Shape {
-    double length;
-    double width;
+    private double length;
+    private double width;
 
-    Rectangle() {
-
-    }
-
-    Rectangle(double length, double width) {
+    public Rectangle(double length, double width) {
+        super();
         this.length = length;
         this.width = width;
     }
 
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
     public double calArea() {
-        return length * width;
+        return getWidth() * getLength();
     }
 }
 
 class Circle extends Shape {
-    double radius;
+    private double radius;
 
-    Circle() {
-
-    }
-
-    Circle(double radius) {
+    public Circle(double radius) {
+        super();
         this.radius = radius;
     }
 
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
     public double calArea() {
-        return 3.14 * radius * radius;
+        double PI = 3.14;
+
+        return PI * getRadius() * getRadius();
     }
 }
