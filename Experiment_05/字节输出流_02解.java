c0fd7d9f3f3133a26c7ce6931c5875917f8bcbb0
Bocchiid <487883857@qq.com>
2025-06-18 11:47:34 +0800

    Add 字节输出流_02解.java

--- Experiment_05/字节输出流_02解.java
+++ Experiment_05/字节输出流_02解.java
@@ -3,21 +3,52 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        /** stream(流) */
+        /** 1.字节流, 2.字符流 */
+        /** 字节流: 以字节的视角来看, 操作二进制的东西 */
+        /** 字符流: 以字符的视角来看, 操作字符 */
+        /** 字符流: 帮你自动将字节转换成了字符 */
+
+        /** FileStream(文件流) */
+        /** stream: inputstream, outstream */
+
+        /** stream(字节流) */
+        /** er(字符流) */
+        /** reader(输入流) writer(输出流) */
 
         try {
-            FileWriter fileWriter = new FileWriter("D:/ss.txt");
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+//            FileReader fileReader = new FileReader("D:/text.txt");
+////
+////            char []s = new char[1024];
+////            int len = 0;
+////
+////            while ((len = fileReader.read(s)) != -1) {
+////                System.out.println(new String(s, 0, len));
+////            }
+////            /** 关闭字符流(好习惯) */
+////            fileReader.close();
 
-            String line;
+//            BufferedReader bufferedReader = new BufferedReader(fileReader);
+//            String line = null;
+//
+//            while ((line = bufferedReader.readLine()) != null) {
+//                System.out.println(line);
+//            }
+
+            Scanner in = new Scanner(System.in);
+
+            FileWriter fileWriter = new FileWriter("D:/ss.txt");
 
             for (int i = 0; i < 2; i++) {
-                line = in.nextLine();
-                bufferedWriter.write(line);
-                bufferedWriter.write("\n");
+                String line = in.nextLine();
+                fileWriter.write(line);
+                fileWriter.write('\n');
             }
+            /** 关闭字符流(好习惯) */
+            fileWriter.close();
 
-            bufferedWriter.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
